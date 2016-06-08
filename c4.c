8faf67e4a62b16401afc891e539071395ddffa20
pinotto <tollotpietro@outlook.com>
2016-06-08 09:26:58 +0200

    commit1

---
+++ c4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void) {
+    printf("Jenkins fa la build alla commit e fa il report del terminale di gradle!\n");
+    return 0;   
+}
+//dasdas
+//8tg86
+//provaprova
+//provaprovaprova
+
+vediamo  prova
+prova
